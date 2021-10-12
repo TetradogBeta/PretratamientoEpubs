41f0e0c10eb334436d92745a4d53908537f85de6
Gabriel <tetradog@gmail.com>
2021-10-12 20:38:17 +0200

    desarrollando primera parte la de añadir Idiomas para trabajar y de interficie y clasificar epubs

---
+++ Pretractament de Epubs/Pretractament de Epubs/Fase0/IdiomaManager.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PretractamentDeEpubs.Fase0
+{
+    /// <summary>
+    /// Lógica de interacción para IdiomaManager.xaml
+    /// </summary>
+    public partial class IdiomaManager : UserControl
+    {
+        public IdiomaManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pretractament de Epubs/Pretractament de Epubs/Fase0/VisorIdioma.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Gabriel.Cat.S.Extension;
+using PretractamentDeEpubs.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PretractamentDeEpubs.Fase0
+{
+    /// <summary>
+    /// Lógica de interacción para VisorIdioma.xaml
+    /// </summary>
+    public partial class VisorIdioma : UserControl
+    {
+        public VisorIdioma()
+        {
+            InitializeComponent();
+        }
+        public Idioma? Idioma { get; set; }
+        public void Refresh()
+        {
+            if (!ReferenceEquals(Idioma, default))
+            {
+                txtNombre.Text = Idioma.Nombre;
+                txtCulture.Text = Idioma.Culture;
+                if (!ReferenceEquals(Idioma.PathIcono, default))
+                {
+                    imgIcono.Source = new BitmapImage(new Uri(Idioma.PathIcono));
+                }
+                else
+                {
+                    imgIcono.SetImage(new Bitmap(1, 1));
+                }
+                if (!ReferenceEquals(Idioma.PathTraduccionApp, default))
+                {
+                    imgHayTraduccionApp.SetImage((Bitmap)Bitmap.FromStream(new MemoryStream(Properties.Resources.estrella)));
+                }
+                else
+                {
+                    imgHayTraduccionApp.SetImage(new Bitmap(1, 1));
+                }
+            }
+        }
+    }
+}
